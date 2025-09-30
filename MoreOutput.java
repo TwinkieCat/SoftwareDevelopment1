6bea9b5c0ec76ca77b2ad77d7a25806a4bdc9d23
Olivier Lukaszewski <85970273+TwinkieCat@users.noreply.github.com>
2025-09-30 16:22:57 +0100

    Add files via upload
    First Software Development Stuff

---
+++ MoreOutput.java
@@ -0,0 +1,18 @@
+public class MoreOutput{
+    public static void main(String[] args){
+        System.out.println("Hello");
+         System.out.println("World");
+          System.out.println("Hello World");
+           System.out.print("Hello ");
+            System.out.print("World");
+             System.out.println("Word1 ");
+              System.out.println("Word2");
+               System.out.print("Word1\nWord2\nWord3\n");
+                System.out.println("Name\tAddress");
+                 System.out.println("====\t=========");
+                  System.out.println("Jim\tTuam");
+}
+
+
+
+}
---
+++ TwentyFourthSeptember.java
@@ -0,0 +1,26 @@
+public class TwentyFourthSeptember {
+
+    public static void main(String[] args){
+
+    //On Wednesday at 3 PM we have Software Development
+    //Decimal number 167.45
+
+    int hour = 3;
+    double decimalNumber = 167.45;
+    String day = "Wednesday";
+
+    System.out.println("On " + day + " at " + hour + " PM we have Software Development");
+    System.out.println("Decimal Number " + decimalNumber);
+
+    hour = 4;
+    decimalNumber = 200.99;
+    day = "Thursday";
+
+    System.out.println("On " + day + " at " + hour + " PM we have Software Development");
+    System.out.println("Decimal Number " + decimalNumber);
+
+    }
+
+    
+    
+}
